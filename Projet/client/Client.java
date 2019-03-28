41926e92f6844fd3ded848607e8a4545912c4224
XAVIER JULIEN <3521024@ppti-14-509-12.ufr-info-p6.jussieu.fr>
2019-03-28 20:17:15 +0100

    A continuer sur server.ml

--- Projet/client/Client.java
+++ Projet/client/Client.java
@@ -1,39 +1,21 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.io.DataInputStream;
-import java.io.PrintStream;
-// public class TestSocket {
-//
-//    public static void main(String[] args) {
-//       try {
-//          Socket sock = new Socket(InetAddress.getByName("127.0.0.1"), Integer.parseInt(args[0]));
-//          String message = args[1];
-//          PrintWriter out = new PrintWriter(sock.getOutputStream());
-//
-//          out.println(name);
-//       } catch (UnknownHostException e) {
-//          e.printStackTrace();
-//       }catch (IOException e) {
-//          e.printStackTrace();
-//       }
-//    }
-// }
-
 
 public class Client {
-  protected static final int PORT=2019;
+  protected static final int PORT=2020;
 
-  public static void main(String[] args) {
+public static void main(String[] args) {
     Socket s=null;
     if (args.length != 1) {
       System.err.println("Usage: java Client <hote>");
       System.exit(1); }
       try {
         s = new Socket (InetAddress.getByName("127.0.0.1"),PORT);
-        DataInputStream canalLecture = new DataInputStream(s.getInputStream());
+        BufferedReader canalLecture = new BufferedReader(new InputStreamReader(s.getInputStream()));
         PrintStream canalEcriture = new PrintStream(s.getOutputStream());
         System.out.println("Connexion etablie : "+
         s.getInetAddress()+" port : "+s.getPort());
@@ -45,9 +27,9 @@ public class Client {
           while ((c=(char)System.in.read()) != '\n') {ligne=ligne+c;}
           canalEcriture.println(ligne);
           canalEcriture.flush();
-          // System.out.println("apres flush");
+          System.out.println("apres flush");
           ligne=canalLecture.readLine();
-          // System.out.println("apres readline");
+          System.out.println("apres readline");
         if (ligne == null) {System.out.println("Connexion terminee"); break;}
         System.out.println("!"+ligne);
         }
--- Projet/server/server.ml
+++ Projet/server/server.ml
@@ -1,56 +1,161 @@
-(* let creer_serveur max_co =
-  let sock = Unix.socket Unix.PF_INET SOCK_STREAM 0
-  and addr = Unix.inet_addr_of_string "127.0.0.1"
-  and port = 2019 in
-    Unix.bind sock (Unix.ADDR_INET(addr, port));
-    Unix.listen sock max_co;
-    sock;; *)
-exception Fin;;
+exception Fin
+exception BadRequest
+exception AlreadyExists
 
-let my_input_line fd =
-  let s = Bytes.create 200 and r = ref "" in
-  try
-  while (Unix.read fd s 0 1 > 0) && (Bytes.get s 0 <> '\n') do
-    r := !r ^ Bytes.to_string s;
-    print_string !r
-  done;
-  !r
-  with Invalid_argument _ -> "il y a un argument invalide"
+let mutex_list_players = Mutex.create (); (*sync for creating player with name*) 
 
 
-let echo_service socket_client =
-  (* let message = Bytes.create 10000
-  and inchan = Unix.in_channel_of_descr socket_client
-  and outchan = Unix.out_channel_of_descr socket_client in *)
-  try
-    while true do
-      let line = my_input_line socket_client in
-      (if (line = "") || (line = "\013") then raise Fin ;
-      print_string line;
-      let result = Bytes.of_string ((String.uppercase_ascii line)^"\n" )in
-      (* let result = Bytes.of_string "Bonsoir" in *)
-        Unix.write socket_client result 0 (Bytes.length result))
-    done
-  with Fin -> ()
-      | exn -> print_string (Printexc.to_string exn) ; print_newline()
+
+type car = {
+	position: float * float;
+	direction: float;
+	speed: float * float
+}
+
+type player = {
+    name: string;
+    socket: Unix.file_descr;
+    inchan: in_channel;
+    outchan: out_channel;
+    mutable score: int;
+    vehicule: car
+    (*mutable playing: status*)
+}
+
+type session = {
+	mutable list_players : player list;
+    mutable playing : bool;
+    mutable objectif : float * float
+	win_cap : int
+}
+
+(*permet de créer un joueur*)
+let create_player user sock inc out = 
+{
+    name = user;
+    socket = sock;
+    inchan = inc;
+    outchan = out;
+    score = 0;
+    vehicule = {position=(0.0,0.0);direction=0.0;speed=(0.0,0.0)}
+    (*playing = WAITING*)
+}
+
+let current_session = 
+  {
+    list_players = [];
+    playing = false;
+	win_cap = 10
+  };;
 
 
-let server_process socket_descr service =
-      Thread.create service socket_descr;
-      print_string "le thread a été crée"
+let parse_request req = 
+  let s = Str.split (Str.regexp "/") req in
+  if (List.length s) > 0 then s else raise BadRequest
+
+let parse_coord c = 
+	let lcoord = Str.split (Str.regexp "X\|Y") c in
+		(float_of_string (List.nth lcoord 0),float_of_string (List.nth lcoord 1))
+ 
+ 
+let rec stringify_scores list_players = 
+	 match list_players with 
+	 	|hd::tl -> if tl <> [] then hd.name":"^hd.score^"|")^(stringify_scores tl)
+	 						   else s^hd.name":"^hd.score^"/"
+	 						   
+
+let make_welcome player = 
+	let phase = if current_session.playing then "playing/" else "waiting/"
+	and scores = stringify_scores current_session.list_players 
+	and coords = let (x,y) = current_session.objectif in 
+	"("^(string_of_float x)^","(string_of_float y)^")" in 
+	output_string player.outchan "WELCOME/"^phase^scores^coords^"/\n";
+	flush player.outchan
+
+(*let make_denied chan =
+	output_string chan "DENIED/";
+	flush chan*) 
+let make_newplayer user_name =
+	let send_fun p = 
+	if p.name<>user_name then 
+	output_string p.outchan "NEWPLAYER/"^user_name^"/\n";
+	flush player.outchan
+	else ()
+
+let find_player user_name = 
+	List.find (fun p -> if p.name=user_name then true else false) current_session.list_players
 
 
-let main () =
+
+
+let process_exit user_name =
+	let rec process_aux l_players = 
+		match l_players with 
+		|{name;_} as hd::tl -> if name=user_name then tl else hd::(process_aux tl) 
+		|[] -> raise NotFound in
+		try 
+			let player = find_player user_name in 
+			Unix.close player.socket;
+			current_session.list_players <- process_aux current_session.list_players;
+			make_playerleft ()
+		with NotFound -> print_endline "trouver quoi faire si cela arrive"
+	
+	
+let process_newpos user_name coord = 
+	let player = find_player user_name in 
+		player.vehicule.position <- parse_coord coord
+
+
+let process_player player = 
+	while true do 
+		let request = input_line player.inchan 
+		and req_parse = parse_request request in 
+			match List.hd res_parse with 
+			|"EXIT" -> process_exit (List.nth req_parse 1)
+			|"NEWPOS" -> process_newpos (List.nth req_parse 1)
+			|_ -> ()
+	done
+				
+
+let process_new_connect client_socket =
+	let inchan = Unix.in_channel_of_descr client_socket 
+	and outchan = Unix.out_channel_of_descr client_socket in
+		let request = input_line inchan in
+		let req_parse = parse_request request in
+			try 
+				match List.hd req_parse with 
+					| "CONNECT" -> 
+						let n = (List.nth req_parse 1) in 
+							Mutex.lock mutex_list_players;
+							if (List.exists (fun {name;_} -> name==n) current_session.list_players) 
+							then raise AlreadyExists
+							else begin
+								let player = (create_player n client_socket inchan outchan) in 
+								current_session.list_players<-player::current_session.list_players;
+								make_welcome player;
+								process_player player
+								end						
+						    Mutex.unlock mutex_list_players
+					| _ -> raise BadRequest
+			with BadRequest -> output_string outchan "DENIED/BadRequest\n";
+							   flush outchan
+				|AlreadyExists -> output_string outchan "DENIED/AlreadyExists\n";
+							   flush outchan
+			 
+let start_server nb_c =
   let server_socket = Unix.socket Unix.PF_INET SOCK_STREAM 0
-  and addr = Unix.inet_addr_of_string "127.0.0.1"
-  and port = 2019 in
+  and addr = Unix.inet_addr_of_string "127.0.0.1" in 
   begin
-    Unix.bind server_socket (Unix.ADDR_INET(addr, port));
-    Unix.listen server_socket (int_of_string Sys.argv.(1));
+  	
+    Unix.bind server_socket (Unix.ADDR_INET(addr, 2019));
+    Unix.listen server_socket nb_c;
     while true do
-      let (client_socket, client_so_addr) = Unix.accept server_socket in
-        server_process client_socket echo_service
+      let (client_socket, _) = Unix.accept server_socket in
+      	print_endline "Nouvelle connexion\n";
+        ignore (Thread.create process_new_connect client_socket);
+        print_endline "Nb"
     done
-  end
+  end;;
 
-let _ = main ();;
+
+start_server (int_of_string Sys.argv.(1));;
